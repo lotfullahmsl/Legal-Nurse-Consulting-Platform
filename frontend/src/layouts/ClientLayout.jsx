f918f80a2a307109d29f826a53df22b7132a86c9
Lotfullah Muslimwal <lotfullahmsl@gmail.com>
2026-02-18 11:39:48 +0500

    getting ready for production

--- frontend/src/layouts/ClientLayout.jsx
+++ frontend/src/layouts/ClientLayout.jsx
@@ -11,7 +11,7 @@ const ClientLayout = ({ children }) => {
             <ClientNavbar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
             <div className="flex">
                 <ClientSidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
-                <main className="flex-1 pt-16 md:pl-64">
+                <main className="flex-1 pt-16 md:pl-64" style={{ marginLeft: '-35vh' }}>
                     {children}
                 </main>
             </div>
